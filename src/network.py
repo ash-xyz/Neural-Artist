74f9a7d56aee4a05ecbbbf3095d9c9c0c4cb0a6e
Ashraf Ali <ashrafali379@gmail.com>
2020-07-01 18:54:33 +0100

    Fresh start

--- src/network.py
+++ src/network.py
@@ -2,23 +2,6 @@ from utilities import *
 import tensorflow as tf
 
 
-def compute_content_loss(content, pastiche):
-    """ Computes the loss of the pastiche with respect to the content
-
-    Args:
-        content: A list of features of the content image, Tensor of shape [1, H, W, C]
-        pastiche: A list of features of the pastische image, Tensor of shape [1, H, W, C]
-
-    Returns:
-        Scalar loss
-    """
-    loss = 0
-    for i in range(len(content)):
-        loss += tf.reduce_sum(
-            tf.math.squared_difference(content[i], pastiche[i]))
-    return loss
-
-
 def gram_matrix(features):
     """Computes the Gram matrix from features
 
@@ -31,41 +14,6 @@ def gram_matrix(features):
     _, H, W, C = tf.shape(features)
     gram = tf.linalg.einsum('bijc,bijd->bcd', features, features)
     return gram/tf.cast(H*W, tf.float32)
-
-
-def compute_style_loss(style_gram, pastiche_features, weights):
-    """Computes the loss of the pastische with respect to the style
-
-    Args:
-        style_gram: A list of Gram tensors of shape [C,C]
-        pastische_features: A list of features with Tensorshape 1, H, W, C
-        weights: A list of size len(style_gram) containing the proportional weight of each layer
-
-    Returns:
-        Scalar loss
-    """
-    loss = 0
-    for i in range(len(pastiche_features)):
-        gram = gram_matrix(pastiche_features[i])
-        loss += weights[i] * \
-            tf.reduce_sum(tf.math.squared_difference(
-                style_gram[i], gram))
-    return loss
-
-
-def tv_loss(img):
-    """
-    Compute total variation loss.
-    Inputs:
-    - img: Tensor of shape (1, H, W, 3) holding an input image.
-    Returns:
-    - loss: Tensor holding a scalar giving the total variation loss
-      for img weighted by tv_weight.
-    """
-    N, H, W, C = img.shape
-    sub_down = img[:, :H-1, :, :] - img[:, 1:H, :, :]
-    sub_right = img[:, :, :W-1, :] - img[:, :, 1:W, :]
-    return (tf.reduce_sum(tf.square(sub_down))) + tf.reduce_sum(tf.square(sub_right))
 
 
 def loss_network():
@@ -88,60 +36,3 @@ def loss_network():
     model = tf.keras.Model(inputs=cnn.input, outputs=outputs)
     divider = len(content_layers)
     return model, divider, weights
-
-
-def pastische_generator(image_path, style_path):
-
-    network, divider, weights = loss_network()
-
-    # load image
-    content_image = load_image(image_path)
-    style_image = load_image(style_path)
-
-    # Initialize content and style features
-    content_features = network(content_image)[:divider]
-    style_gram = network(style_image)[divider:]
-    for i in range(len(style_gram)):
-        style_gram[i] = gram_matrix(style_gram[i])
-
-    # Set up optimization hyperparameters
-    initial_lr = 3.0
-    decayed_lr = 0.1
-    decay_lr_at = 180
-    max_iter = 1000
-
-    step = tf.Variable(0, trainable=False)
-    boundaries = [decay_lr_at]
-    values = [initial_lr, decayed_lr]
-    learning_rate_fn = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
-        boundaries, values)
-
-    # Initialize optimizer
-    learning_rate = learning_rate_fn(step)
-    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-2)
-
-    # Initialize Pastische as the content image
-    pastische = tf.Variable(content_image)
-
-    for i in range(max_iter):
-        with tf.GradientTape() as tape:
-            tape.watch(pastische)
-            pastische_features = network(pastische)
-            # Compute Loss
-            content_loss = compute_content_loss(
-                content_features, pastische_features)
-            style_loss = compute_style_loss(
-                style_gram, pastische_features[divider:], weights)
-            #tv = tv_loss(pastische)
-            loss = 1e4*content_loss + 1e-2*style_loss #+ 1e-3*tv
-        # Apply gradient decent
-        grad = tape.gradient(loss, pastische)
-        optimizer.apply_gradients([(grad, pastische)])
-
-        if i % 10 == 0:
-            print(f"Number of Iterations: {i}")
-    return pastische
-
-
-pastische = pastische_generator("obrien.jpg", "style.jpg")
-save_image(pastische, "pastische.jpg")
