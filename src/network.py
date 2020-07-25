ff59e4651920b3c54399374751bac0617cb6ead3
Ashraf Ali <ashrafali379@gmail.com>
2020-07-25 23:05:18 +0100

    Changed loss network to take from the hyperparameters

--- src/network.py
+++ src/network.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from src.hyperparameters import CONTENT_LAYERS, STYLE_LAYERS
 """
 File that contains the loss and style networks
 """
@@ -8,19 +9,11 @@ def loss_network():
     """Generates a loss network
 
     Returns:
-        model: keras functional api based on VGG19
-        len_content: length of the content layers
-        len_style: length of the style layers
+        model: keras functional api based on VGG19 with content and style layers outputs
     """
     cnn = tf.keras.applications.VGG19(
         include_top=False, weights='imagenet')
-    content_layers = ['block3_conv2']
-    style_layers = [
-        'block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1'
-    ]
-    layers = content_layers+style_layers
+    layers = CONTENT_LAYERS+STYLE_LAYERS
     outputs = [cnn.get_layer(name).output for name in layers]
     model = tf.keras.Model(inputs=cnn.input, outputs=outputs)
-    len_content = len(content_layers)
-    len_styles = len(style_layers)
-    return model, len_content, len_styles
+    return model
