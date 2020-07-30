d52f312311f2463bce4d1c8cd477a9a1023c67ba
Ashraf Ali <ashrafali379@gmail.com>
2020-07-30 20:13:19 +0100

    Added visualize function to find the styles each layer of the loss network produces

--- src/network.py
+++ src/network.py
@@ -1,19 +1,22 @@
 import tensorflow as tf
-from .config import CONTENT_LAYERS, STYLE_LAYERS
 """
 File that contains the loss and style networks
 """
 
 
-def loss_network():
-    """Generates a loss network
+def loss_network(content_layers, style_layers):
+    """Generates a loss network based on vgg 19
+
+    Args:
+        content_layers: layers wanted for the content
+        style_layers: layers wanted for the style
 
     Returns:
         model: keras functional api based on VGG19 with content and style layers outputs
     """
     cnn = tf.keras.applications.VGG19(
         include_top=False, weights='imagenet')
-    layers = CONTENT_LAYERS+STYLE_LAYERS
+    layers = content_layers+style_layers
     outputs = [cnn.get_layer(name).output for name in layers]
     model = tf.keras.Model(inputs=cnn.input, outputs=outputs)
     return model
--- src/utilities.py
+++ src/utilities.py
@@ -19,6 +19,31 @@ def load_image(image_path):
     return image
 
 
+def load_and_process_image(path_to_image):
+    image = load_image(path_to_image)
+    image = tf.keras.applications.vgg19.preprocess_input(image)
+    return image
+
+
+def deprocess_image(processed_image):
+    x = processed_image.copy()
+    if len(x.shape) == 4:
+        x = np.squeeze(x, 0)
+    assert len(x.shape) == 3, ("Input to deprocess image must be an image of "
+                               "dimension [1, height, width, channel] or [height, width, channel]")
+    if len(x.shape) != 3:
+        raise ValueError("Invalid input to deprocessing image")
+
+    # perform the inverse of the preprocessing step
+    x[:, :, 0] += 103.939
+    x[:, :, 1] += 116.779
+    x[:, :, 2] += 123.68
+    x = x[:, :, ::-1]
+
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+
 def save_image(tensor, name):
     """Turns a tensor into an image
 
