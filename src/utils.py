5dca2e30880f0805cc87b222d7713e12cf76f573
Ashraf Ali <ashrafali379@gmail.com>
2020-08-17 00:16:56 +0100

    Changes 🌈

--- src/utils.py
+++ src/utils.py
@@ -1,34 +1,28 @@
 import torch
-from PIL import Image
-
-
-def load_image(filename, size=None, scale=None):
-    img = Image.open(filename)
-    if size is not None:
-        img = img.resize((size, size), Image.ANTIALIAS)
-    elif scale is not None:
-        img = img.resize((int(img.size[0] / scale), int(img.size[1] / scale)), Image.ANTIALIAS)
-    return img
-
-
-def save_image(filename, data):
-    img = data.clone().clamp(0, 255).numpy()
-    img = img.transpose(1, 2, 0).astype("uint8")
-    img = Image.fromarray(img)
-    img.save(filename)
-
-
-def gram_matrix(y):
-    (b, ch, h, w) = y.size()
-    features = y.view(b, ch, w * h)
-    features_t = features.transpose(1, 2)
-    gram = features.bmm(features_t) / (ch * h * w)
-    return gram
-
 
 def normalize_batch(batch):
-    # normalize using imagenet mean and std
+    """Normalize batch using ImageNet mean and std
+    Args:
+        batch: batch of images, shape: b, ch, h, w
+    Returns:
+        Normalized batch
+    """
     mean = batch.new_tensor([0.485, 0.456, 0.406]).view(-1, 1, 1)
     std = batch.new_tensor([0.229, 0.224, 0.225]).view(-1, 1, 1)
     batch = batch.div_(255.0)
+
     return (batch - mean) / std
+
+
+def gram_matrix(tensor):
+    """Computes the gram matrix for a tensor
+    Args:
+        tensor: A tensor of shape b, ch, h, w; Where b is the batch size
+    Returns:
+        gram: The Calculated gram matrix of shape b, ch, h*w
+    """
+    (batch, channel, height, width) = tensor.size()
+    features = tensor.view(batch, channel, height * width)
+    features_t = features.transpose(1, 2)
+    gram = features.bmm(features_t) / (channel * height * width)
+    return gram
