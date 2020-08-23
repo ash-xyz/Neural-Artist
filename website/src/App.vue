ab0c26564c75e2cea426d4558c5e2bbbdab15604
Ashraf Ali <ashrafali379@gmail.com>
2020-08-23 12:50:55 +0100

    Added Website element

---
+++ website/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Github />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Github from "./components/Github.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Github
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
