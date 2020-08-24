c5c1346f80cbfc019d938d9acca4866a1def0b03
Ashraf Ali <ashrafali379@gmail.com>
2020-08-24 20:34:09 +0100

    Added Interpolation layer

--- src/eval.py
+++ src/eval.py
@@ -9,12 +9,15 @@ from src.utils import save_image, load_image
 def stylize(args):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    # Loads image
     content = load_image(args.content).to(device)
 
     with torch.no_grad():
+        # Load Transformer net
         style_model = TransformerNet()
         state_dict = torch.load(args.model)
 
+        # Load Model Weights
         style_model.load_state_dict(state_dict)
         style_model.to(device)
 
---
+++ src/export.py
@@ -0,0 +1,1 @@
+"""Exports trained models to be used as .onnx files"""
