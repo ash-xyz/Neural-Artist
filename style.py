be0903af8274a3782baf4ecc42cd26577aea162b
Ashraf Ali <ashrafali379@gmail.com>
2020-08-24 14:02:07 +0100

    Added video parser

--- style.py
+++ style.py
@@ -5,6 +5,7 @@ import sys
 from src.train import train
 from src.eval import stylize
 from src.cam import webcam
+from src.video import transform_video
 
 if __name__ == '__main__':
     main_parser = argparse.ArgumentParser()
@@ -22,10 +23,12 @@ if __name__ == '__main__':
                                   help='Size of training images')
     train_arg_parser.add_argument('--content-weight', type=float, default=1,
                                   help='Weight for content loss')
-    train_arg_parser.add_argument('--style-weight', type=float, default=220,
+    train_arg_parser.add_argument('--style-weight', type=float, default=200,
                                   help='Weight for style loss')
-    train_arg_parser.add_argument('--tv-weight', type=float, default=1e-2,
+    train_arg_parser.add_argument('--tv-weight', type=float, default=1e-6,
                                   help='Weight for tv loss')
+    train_arg_parser.add_argument('--norm-range', type=bool, default=True,
+                                  help='Normalization Range for VGG input, True for 0,255 False for 0,1')
 
     train_arg_parser.add_argument('--epochs', type=int, default=2)
     train_arg_parser.add_argument('--batch-size', type=int, default=4)
@@ -50,6 +53,16 @@ if __name__ == '__main__':
     cam_arg_parser.add_argument('--width', type=int, default=None)
     cam_arg_parser.add_argument('--height', type=int, default=None)
 
+    vid_arg_parser = subparser.add_parser("video", help="parser for videos")
+    vid_arg_parser.add_argument('--model', type=str, required=True,
+                                help="Path to style model")
+    vid_arg_parser.add_argument('--output-dir', type=str, required=True,
+                                help='Path to the result video')
+    vid_arg_parser.add_argument('--content', type=str, required=True,
+                                help='Path to the content video')
+    vid_arg_parser.add_argument('--show-frame', type=bool, default=False,
+                                help='Shows current frame being processed')
+
     args = main_parser.parse_args()
 
     if args.subcommand is None:
@@ -63,3 +76,5 @@ if __name__ == '__main__':
         stylize(args)
     if args.subcommand == "cam":
         webcam(args)
+    if args.subcommand == "video":
+        transform_video(args)
